56896131f1ecb54825d31fb960907911414b7da1
Pankaj Agarwal <quasar.pankaj@gmail.com>
2024-04-08 17:33:39 +0530

    added debugging

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -32,6 +32,8 @@ return {
   config = function()
     local dap, dapui = require("dap"), require("dapui")
 
+      dapui.setup()
+
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end

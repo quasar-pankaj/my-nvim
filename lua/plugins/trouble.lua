0467c0742d5abe58cfb6fb5b687cc6a153d708c6
Pankaj Agarwal <quasar.pankaj@gmail.com>
2024-10-09 11:43:17 +0530

    resolved errors

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,19 +1,37 @@
 return {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
   },
-  config = function()
-    local trouble = require("trouble")
-    -- Lua
-    vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
-    vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end)
-   vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
-    vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
-    vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
-    vim.keymap.set("n", "gR", function() trouble.toggle("lsp_references") end)
-  end
 }
